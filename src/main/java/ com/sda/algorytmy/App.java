05489968e198676ff63b6c0eb3b4a8845d99546e
bajserowiczMikolaj <mbajserowicz@gmail.com>
2019-05-12 14:06:54 +0100

    002

--- src/main/java/ com/sda/algorytmy/App.java
+++ src/main/java/ com/sda/algorytmy/App.java
@@ -8,6 +8,26 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        int[] a ={3,6,7,34,55};
+        int wyniki= a[0];
+        int maximum = a[0];
+
+        for(int i=1;i<a.length;i++){
+            if(a[i]>wyniki){
+                wyniki = a[i];
+            }
+        }
+
+
+        for(int i=1;i<a.length;i++){
+            if(a[i]<maximum){
+                wyniki = a[i];
+            }
+        }
+
+
+
+        System.out.println("element minimalny:" + maximum);
+        System.out.println("element maxymalny:" + wyniki);
     }
 }
